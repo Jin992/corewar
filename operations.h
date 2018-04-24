260b4464979291a2ef793356ca8880a19201edab
Andrii YAVORSKYI <ayavorsk@e2r12p12.unit.ua>
2018-04-24 18:31:36 +0300

    move

---
+++ operations.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayavorsk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/24 17:32:57 by ayavorsk          #+#    #+#             */
+/*   Updated: 2018/04/24 17:32:59 by ayavorsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef OPERATIONS_H
+# define OPERATIONS_H
+
+void	add_operation();
+void	aff_operation();
+void	and_operation();
+void	fork_operation();
+void 	ld_operation();
+void	ldi_operation();
+void	lfork_operation();
+void	live_operation();
+void 	lld_operation();
+void	lldi_operation();
+void 	or_operation();
+void 	st_operation();
+void 	sti_operation();
+void 	sub_operation();
+void	xor_operation();
+void	zjmp_operation();
+
+#endif
